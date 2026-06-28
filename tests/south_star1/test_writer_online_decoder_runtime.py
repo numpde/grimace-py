18b6aac6aacdb2f498f25b4ef009b4b1e2dc2385
numpde <21158052+numpde@users.noreply.github.com>
2026-06-28 22:22:45 +0300

    Update writer online tests for dedicated decoder type

--- tests/south_star1/test_writer_online_decoder_runtime.py
+++ tests/south_star1/test_writer_online_decoder_runtime.py
@@ -4,16 +4,15 @@ from __future__ import annotations
 
 import unittest
 
-from grimace._south_star1.online_decoder_api import SouthStarOnlineDecoderState
-from grimace._south_star1.online_decoder_api import WriterRuntimeOnlineStats
 from grimace._south_star1.online_decoder_api import make_branch_preserving_online_decoder
 from grimace._south_star1.online_decoder_api import make_determinized_online_decoder
-from grimace._south_star1.online_decoder_state import OnlineDecoderState
 from grimace._south_star1.policy import SerializationLanguageMode
 from grimace._south_star1.prepared_runtime import SouthStarRuntimeOptions
 from grimace._south_star1.prepared_runtime import SouthStarWriterSurface
 from grimace._south_star1.prepared_runtime import enumerate_prepared_writer_shaped_support
 from grimace._south_star1.prepared_runtime import prepare_south_star_mol_from_facts
+from grimace._south_star1.writer_online_decoder import WriterRuntimeOnlineStats
+from grimace._south_star1.writer_online_decoder import WriterShapedOnlineDecoderState
 from grimace._south_star1.writer_online_decoder import make_writer_shaped_online_decoder
 from grimace._south_star1.writer_runtime import WriterRuntimeState
 from tests.south_star1.helpers import cco_facts
@@ -76,9 +75,9 @@ class WriterOnlineDecoderRuntimeTest(unittest.TestCase):
     def test_writer_shaped_route_never_falls_through_to_legacy_raw_state(self) -> None:
         prepared = _prepare(cco_facts())
         decoder = make_writer_shaped_online_decoder(prepared=prepared)
-        invalid_state = SouthStarOnlineDecoderState(
+        invalid_state = WriterShapedOnlineDecoderState(
             prefix="",
-            raw_state=OnlineDecoderState(prefix=""),
+            raw_state=object(),
             decoder=decoder,
         )
 
